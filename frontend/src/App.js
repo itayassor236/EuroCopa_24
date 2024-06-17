dbbcd3dfbe98b3822090ece59fe72ff208fe55bb
itayassor236 <itayasor236@gmail.com>
2024-06-17 14:53:42 +0300

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import LandingPage from './components/LandingPage';
 import Euro2024 from './components/Euro2024';
